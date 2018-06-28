37f688b4eb1385088bcc23adf08f98ff5c3fadff
Grigi <nagrigoriadis@gmail.com>
2018-06-28 16:21:37 +0200

    Green theme and formatting fix

--- docs/conf.py
+++ docs/conf.py
@@ -97,7 +97,7 @@ import cloud_sptheme as csp
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'cloud'
+html_theme = 'greencloud'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
