6f11492afe68d23a9040277ff1256ec49f60ed78
Grigi <nagrigoriadis@gmail.com>
2018-06-30 11:03:13 +0200

    Initial port of examples test, need test isolation still

---
+++ tortoise/tests/test_relations.py
@@ -0,0 +1,75 @@
+import uuid
+
+import asynctest
+
+from tortoise import Tortoise
+from tortoise.backends.sqlite.client import SqliteClient
+from tortoise.exceptions import NoValuesFetched
+from tortoise.tests.testmodels import Event, Team, Tournament
+from tortoise.utils import generate_schema
+
+
+class TestRelations(asynctest.TestCase):
+    use_default_loop = True
+
+    async def setUp(self):
+        self.db = SqliteClient('/tmp/test-{}.sqlite'.format(uuid.uuid4().hex))
+        await self.db.create_connection()
+        Tortoise.init(self.db)
+        await generate_schema(self.db)
+
+    async def tearDown(self):
+        await self.db.close()
+
+    @asynctest.strict
+    async def test_tourment(self):
+        tournament = Tournament(name='New Tournament')
+        await tournament.save()
+        await Event(name='Without participants', tournament_id=tournament.id).save()
+        event = Event(name='Test', tournament_id=tournament.id)
+        await event.save()
+        participants = []
+        for i in range(2):
+            team = Team(name='Team {}'.format(i + 1))
+            await team.save()
+            participants.append(team)
+        await event.participants.add(participants[0], participants[1])
+        await event.participants.add(participants[0], participants[1])
+
+        try:
+            for team in event.participants:
+                print(team.id)
+        except NoValuesFetched:
+            pass
+
+        async for team in event.participants:
+            print(team.id)
+
+        for team in event.participants:
+            print(team.id)
+
+        assert event.participants[0].id == participants[0].id
+
+        selected_events = await Event.filter(
+            participants=participants[0].id
+        ).prefetch_related('participants', 'tournament')
+        assert len(selected_events) == 1
+        assert selected_events[0].tournament.id == tournament.id
+        assert len(selected_events[0].participants) == 2
+        await participants[0].fetch_related('events')
+        assert participants[0].events[0] == event
+
+        await Team.fetch_for_list(participants, 'events')
+
+        await Team.filter(events__tournament__id=tournament.id)
+
+        await Event.filter(tournament=tournament)
+
+        await Tournament.filter(events__name__in=['Test', 'Prod']
+                                ).order_by('-events__participants__name').distinct()
+
+        result = await Event.filter(id=event.id).values('id', 'name', tournament='tournament__name')
+        assert result[0]['tournament'] == tournament.name
+
+        result = await Event.filter(id=event.id).values_list('id', 'participants__name')
+        assert len(result) == 2
---
+++ tortoise/tests/testmodels.py
@@ -0,0 +1,33 @@
+"""
+This is the testing Models
+"""
+from tortoise import fields
+from tortoise.models import Model
+
+
+class Tournament(Model):
+    id = fields.IntField(pk=True)
+    name = fields.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Event(Model):
+    id = fields.IntField(pk=True)
+    name = fields.TextField()
+    tournament = fields.ForeignKeyField('models.Tournament', related_name='events')
+    participants = fields.ManyToManyField(
+        'models.Team', related_name='events', through='event_team'
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Team(Model):
+    id = fields.IntField(pk=True)
+    name = fields.TextField()
+
+    def __str__(self):
+        return self.name
