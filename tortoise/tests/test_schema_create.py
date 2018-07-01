550c3728009dc99d45f2e48b9cae8d8dbadd1572
Grigi <nagrigoriadis@gmail.com>
2018-07-01 21:12:54 +0200

    converted schema test to unittest assert style

--- tortoise/tests/test_schema_create.py
+++ tortoise/tests/test_schema_create.py
@@ -11,14 +11,14 @@ class TestSchemaCreate(TestCase):
     @asynctest.strict
     async def test_schema_create(self):
         tournament = await Tournament.create(name='Test')
-        assert tournament.created and tournament.created.date() == datetime.date.today()
+        self.assertEquals(tournament.created and tournament.created.date(), datetime.date.today())
 
         event = await Event.create(name='Test 1', tournament=tournament, prize=100)
         old_time = event.modified
         event.name = 'Test 2'
         await event.save()
-        assert event.modified > old_time
-        assert len(event.token) == 32
+        self.assertGreater(event.modified, old_time)
+        self.assertEquals(len(event.token), 32)
         await Team.create(name='test')
         result = await Team.all().values('name')
-        assert result[0]['name'] == 'test'
+        self.assertEquals(result[0]['name'], 'test')
