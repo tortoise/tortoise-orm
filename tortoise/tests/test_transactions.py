6217074d448897cb622ad6209b7e87bfad96a174
Grigi <nagrigoriadis@gmail.com>
2018-07-01 20:26:13 +0200

    fixed transaction test

--- tortoise/tests/test_transactions.py
+++ tortoise/tests/test_transactions.py
@@ -1,26 +1,24 @@
 import sqlite3
-import unittest
 
 import asynctest
 
 from tortoise.contrib.testing import TestCase
-from tortoise.tests.testmodels import Event
+from tortoise.tests.testmodels import Tournament
 
 
 class TestTransactions(TestCase):
 
-    @unittest.expectedFailure
     @asynctest.strict
     async def test_transactions(self):
         try:
             async with self.db.in_transaction() as connection:
-                event = Event(name='Test')
+                event = Tournament(name='Test')
                 await event.save(using_db=connection)
-                await Event.filter(id=event.id).using_db(connection).update(name='Updated name')
-                saved_event = await Event.filter(name='Updated name').using_db(connection).first()
+                await Tournament.filter(id=event.id).using_db(connection).update(name='Updated name')
+                saved_event = await Tournament.filter(name='Updated name').using_db(connection).first()
                 assert saved_event.id == event.id
                 await connection.execute_query('SELECT * FROM non_existent_table')
         except sqlite3.OperationalError:
             pass
-        saved_event = await Event.filter(name='Updated name').first()
+        saved_event = await Tournament.filter(name='Updated name').first()
         assert not saved_event
