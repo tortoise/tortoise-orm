e25d14278eb5337c72d0b858dc1a136488080bbe
Andrey Bondar <andrey@bondar.ru>
2018-12-27 17:45:09 +0300

    Fixed across transactions tests

--- tortoise/tests/test_basic.py
+++ tortoise/tests/test_basic.py
@@ -4,16 +4,19 @@ from tortoise.tests.testmodels import Tournament
 
 class TestBasic(test.TestCase):
     async def test_basic(self):
-        event = await Tournament.create(name='Test')
-        await Tournament.filter(id=event.id).update(name='Updated name')
+        tournament = await Tournament.create(name='Test')
+        await Tournament.filter(id=tournament.id).update(name='Updated name')
         saved_event = await Tournament.filter(name='Updated name').first()
-        self.assertEqual(saved_event.id, event.id)
+        self.assertEqual(saved_event.id, tournament.id)
         await Tournament(name='Test 2').save()
         self.assertEqual(
             await Tournament.all().values_list('id', flat=True),
-            [event.id, event.id + 1]
+            [tournament.id, tournament.id + 1]
         )
         self.assertEqual(
             await Tournament.all().values('id', 'name'),
-            [{'id': event.id, 'name': 'Updated name'}, {'id': event.id + 1, 'name': 'Test 2'}]
+            [
+                {'id': tournament.id, 'name': 'Updated name'},
+                {'id': tournament.id + 1, 'name': 'Test 2'},
+            ],
         )
