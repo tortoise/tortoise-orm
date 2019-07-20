bf02145b47c29f07a30a634a24983bc76dc9e040
Terra Brown <superloach42@gmail.com>
2019-07-20 00:23:18 -0400

    Create test__models__good.py

---
+++ tortoise/tests/test__models__good.py
@@ -0,0 +1,38 @@
+"""
+'Success' tests for __models__
+"""
+
+from tortoise import fields
+from tortoise.models import Model
+from tortoise.tests.testfields import EnumField
+
+
+class BadTournament(Model):
+    id = fields.IntField(pk=True)
+    name = fields.TextField()
+    created = fields.DatetimeField(auto_now_add=True, index=True)
+
+    def __str__(self):
+        return self.name
+
+
+class GoodTournament(Model):
+    id = fields.IntField(pk=True)
+    name = fields.TextField()
+    created = fields.DatetimeField(auto_now_add=True, index=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Tmp:
+    class InAClassTournament(Model):
+        id = fields.IntField(pk=True)
+        name = fields.TextField()
+        created = fields.DatetimeField(auto_now_add=True, index=True)
+
+        def __str__(self):
+            return self.name
+
+
+__models__ = [GoodTournament, Tmp.InAClassTournament]
