44b4502d37e7259be881034c51f45b9b19777cd7
Terra Brown <superloach42@gmail.com>
2019-07-20 00:24:06 -0400

    Create test__models__.py

---
+++ tortoise/tests/test__models__.py
@@ -0,0 +1,71 @@
+"""
+Tests for __models__
+"""
+
+import re
+
+from asynctest.mock import CoroutineMock, patch
+
+from tortoise import Tortoise
+from tortoise.contrib import test
+from tortoise.exceptions import ConfigurationError
+from tortoise.utils import generate_post_table_sql, get_schema_sql
+
+
+class TestGenerateSchema(test.SimpleTestCase):
+    async def setUp(self):
+        try:
+            Tortoise.apps = {}
+            Tortoise._connections = {}
+            Tortoise._inited = False
+        except ConfigurationError:
+            pass
+        Tortoise._inited = False
+        self.sqls = ""
+        self.engine = test.getDBConfig(app_label="models", modules=[])["connections"]["models"][
+            "engine"
+        ]
+
+    async def tearDown(self):
+        Tortoise._connections = {}
+        await Tortoise._reset_apps()
+
+    async def init_for(self, module: str, safe=False) -> None:
+        if self.engine != "tortoise.backends.sqlite":
+            raise test.SkipTest("sqlite only")
+        with patch(
+            "tortoise.backends.sqlite.client.SqliteClient.create_connection", new=CoroutineMock()
+        ):
+            await Tortoise.init(
+                {
+                    "connections": {
+                        "default": {
+                            "engine": "tortoise.backends.sqlite",
+                            "credentials": {"file_path": ":memory:"},
+                        }
+                    },
+                    "apps": {"models": {"models": [module], "default_connection": "default"}},
+                }
+            )
+            self.sqls = get_schema_sql(Tortoise._connections["default"], safe).split("; ")
+            self.post_sqls = generate_post_table_sql(Tortoise._connections["default"], safe).split(
+                "; "
+            )
+
+    def get_sql(self, text: str) -> str:
+        return re.sub(r"[ \t\n\r]+", " ", [sql for sql in self.sqls if text in sql][0])
+
+    def get_post_sql(self, text: str) -> str:
+        return re.sub(r"[ \t\n\r]+", " ", [sql for sql in self.post_sqls if text in sql][0])
+
+    async def test_good(self):
+        await self.init_for("tortoise.tests.test__models__good")
+        self.assertIn('goodtournament', '; '.join(self.sqls))
+        self.assertIn('inaclasstournament', '; '.join(self.sqls))
+        self.assertNotIn('badtournament', '; '.join(self.sqls))
+
+    async def test_bad(self):
+        await self.init_for("tortoise.tests.test__models__bad")
+        self.assertNotIn('goodtournament', '; '.join(self.sqls))
+        self.assertNotIn('inaclasstournament', '; '.join(self.sqls))
+        self.assertIn('badtournament', '; '.join(self.sqls))
