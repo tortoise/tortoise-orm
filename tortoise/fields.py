b84b952f9ccf47c2ab89c428a5a7b68c6b86e90c
wallneradam <adam.wallner@gmail.com>
2019-10-09 20:12:05 +0200

    1 line condition with 'is' testing

--- tortoise/fields.py
+++ tortoise/fields.py
@@ -28,13 +28,12 @@ JSON_LOADS = json.loads
 
 class _FieldMeta(type):
     def __new__(mcs, name, bases, attrs):
-        if len(bases) > 1:
-            if bases[0] == Field:
-                # Instantiate class with only the 1st base class (should be Field)
-                cls = type.__new__(mcs, name, (bases[0],), attrs)  # type: Type[Field]
-                # All other base classes are our meta types, we store them in class attributes
-                cls.type = bases[1] if len(bases) == 2 else bases[1:]
-                return cls
+        if len(bases) > 1 and bases[0] is Field:
+            # Instantiate class with only the 1st base class (should be Field)
+            cls = type.__new__(mcs, name, (bases[0],), attrs)  # type: Type[Field]
+            # All other base classes are our meta types, we store them in class attributes
+            cls.type = bases[1] if len(bases) == 2 else bases[1:]
+            return cls
         return type.__new__(mcs, name, bases, attrs)
 
 
