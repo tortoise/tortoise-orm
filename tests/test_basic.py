d58fb4fedacc5ded48e86b9cf43b8a598abf1a4f
Grigi <nagrigoriadis@gmail.com>
2019-11-04 22:38:19 +0200

    Remove duplicate test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -19,20 +19,3 @@ class TestBasic(test.TestCase):
                 {"id": tournament.id + 1, "name": "Test 2"},
             ],
         )
-
-    async def test_basic2(self):
-        tournament = await Tournament.create(name="Test")
-        await Tournament.filter(id=tournament.id).update(name="Updated name")
-        saved_event = await Tournament.filter(name="Updated name").first()
-        self.assertEqual(saved_event.id, tournament.id)
-        await Tournament(name="Test 2").save()
-        self.assertEqual(
-            await Tournament.all().values_list("id", flat=True), [tournament.id, tournament.id + 1]
-        )
-        self.assertEqual(
-            await Tournament.all().values("id", "name"),
-            [
-                {"id": tournament.id, "name": "Updated name"},
-                {"id": tournament.id + 1, "name": "Test 2"},
-            ],
-        )
