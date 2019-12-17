b8e9b03137f98336e84f3dab9a00871d9a84afab
Grigi <nagrigoriadis@gmail.com>
2019-12-17 15:04:14 +0200

    Added tests that replicate the get_or_create race condition (#140)

--- tests/test_concurrency.py
+++ tests/test_concurrency.py
@@ -1,7 +1,7 @@
 import asyncio
 import sys
 
-from tests.testmodels import Tournament
+from tests.testmodels import Tournament, UniqueName
 from tortoise.contrib import test
 from tortoise.transactions import in_transaction
 
@@ -21,6 +21,21 @@ class TestConcurrencyIsolated(test.IsolatedTestCase):
     async def create_trans_concurrent(self):
         async with in_transaction():
             await asyncio.gather(*[Tournament.create(name="Test") for _ in range(100)])
+
+    async def test_nonconcurrent_get_or_create(self):
+        unas = [await UniqueName.get_or_create(name="c") for _ in range(10)]
+        una_created = [una[1] for una in unas if una[1] is True]
+        self.assertEqual(len(una_created), 1)
+        for una in unas:
+            self.assertEqual(una[0], unas[0][0])
+
+    @test.expectedFailure
+    async def test_concurrent_get_or_create(self):
+        unas = await asyncio.gather(*[UniqueName.get_or_create(name="d") for _ in range(10)])
+        una_created = [una[1] for una in unas if una[1] is True]
+        self.assertEqual(len(una_created), 1)
+        for una in unas:
+            self.assertEqual(una[0], unas[0][0])
 
     @test.skipIf(sys.version_info < (3, 7), "aiocontextvars backport not handling this well")
     @test.requireCapability(supports_transactions=True)
@@ -53,3 +68,18 @@ class TestConcurrencyTransactioned(test.TestCase):
         all_write = await asyncio.gather(*[Tournament.create(name="Test") for _ in range(100)])
         all_read = await Tournament.all()
         self.assertEqual(set(all_write), set(all_read))
+
+    async def test_nonconcurrent_get_or_create(self):
+        unas = [await UniqueName.get_or_create(name="a") for _ in range(10)]
+        una_created = [una[1] for una in unas if una[1] is True]
+        self.assertEqual(len(una_created), 1)
+        for una in unas:
+            self.assertEqual(una[0], unas[0][0])
+
+    @test.skip("Crashes with MySQL & PostgreSQL")
+    async def test_concurrent_get_or_create(self):
+        unas = await asyncio.gather(*[UniqueName.get_or_create(name="b") for _ in range(10)])
+        una_created = [una[1] for una in unas if una[1] is True]
+        self.assertEqual(len(una_created), 1)
+        for una in unas:
+            self.assertEqual(una[0], unas[0][0])
