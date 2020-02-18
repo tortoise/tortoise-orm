662c32bfd89fda68eb8a5d379a29ec445ba3a7be
Sang-Heon Jeon <ekffu200098@gmail.com>
2020-02-18 14:45:52 +0900

    Fix join query with examples

--- examples/relations_with_unique.py
+++ examples/relations_with_unique.py
@@ -8,6 +8,7 @@ to get this related objects
 import logging
 
 from tortoise import Tortoise, fields, run_async
+from tortoise.functions import Count
 from tortoise.models import Model
 
 logging.basicConfig(level=logging.DEBUG)
@@ -25,7 +26,7 @@ class Student(Model):
     id = fields.IntField(pk=True)
     name = fields.TextField()
     school: fields.ForeignKeyRelation[School] = fields.ForeignKeyField(
-        "models.School", related_name="students", to_field="id",
+        "models.School", related_name="students", to_field="id"
     )
 
 
@@ -34,10 +35,13 @@ async def run():
     await Tortoise.generate_schemas()
 
     school = await School.create(id=1024, name="School")
-    student = await Student.create(name="Sang-Heon Jeon", school=school)
+    student1 = await Student.create(name="Sang-Heon Jeon1", school=school)
+    student2 = await Student.create(name="Sang-Heon Jeon2", school_id=school.id)
 
-    student = await Student.get(id=student.id)
-    print(await student.school)
+    await School.filter(students__id=student1.id).values("id", "name", student="students__name")
+    await Student.filter(id=student2.id).values("id", "name", school="school__name")
+    queryset = School.all().annotate(count=Count("students"))
+    await queryset.filter(name="School").first()
 
 
 if __name__ == "__main__":
