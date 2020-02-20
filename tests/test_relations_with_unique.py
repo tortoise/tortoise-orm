6cb0a003ef036ef6f616bcc7fe477b43c22dc40f
Sang-Heon Jeon <ekffu200098@gmail.com>
2020-02-20 17:45:07 +0900

    Finalize test suite with integrity test suite

--- tests/test_relations_with_unique.py
+++ tests/test_relations_with_unique.py
@@ -1,13 +1,42 @@
-from tests.testmodels import School, Student
+from tests.testmodels import Principal, School, Student
 from tortoise.contrib import test
+from tortoise.query_utils import Prefetch
 
 
 class TestRelationsWithUnique(test.TestCase):
     async def test_relation_with_unique(self):
-        school = await School.create(id=1024, name="School")
-        await Student.create(name="Sang-Heon Jeon", school_id=school.id)
 
+        school1 = await School.create(id=1024, name="School1")
+        student1 = await Student.create(name="Sang-Heon Jeon1", school_id=school1.id)
+
+        student_schools = await Student.filter(name="Sang-Heon Jeon1").values(
+            "name", "school__name"
+        )
+        self.assertEqual(student_schools[0], {"name": "Sang-Heon Jeon1", "school__name": "School1"})
         student_schools = await Student.all().values(school="school__name")
-        self.assertEqual(student_schools[0]["school"], school.name)
+
+        self.assertEqual(student_schools[0]["school"], school1.name)
         student_schools = await Student.all().values_list("school__name")
-        self.assertEqual(student_schools[0][0], school.name)
+        self.assertEqual(student_schools[0][0], school1.name)
+
+        await Student.create(name="Sang-Heon Jeon2", school=school1)
+        school_with_filtered = (
+            await School.all()
+            .prefetch_related(Prefetch("students", queryset=Student.filter(name="Sang-Heon Jeon1")))
+            .first()
+        )
+        school_without_filtered = await School.first().prefetch_related("students")
+        self.assertEqual(len(school_with_filtered.students), 1)
+        self.assertEqual(len(school_without_filtered.students), 2)
+
+        student_direct_prefetch = await Student.first().prefetch_related("school")
+        self.assertEqual(student_direct_prefetch.school.id, school1.id)
+
+        school2 = await School.create(id=2048, name="School2")
+        await Student.all().update(school=school2)
+        student = await Student.first()
+        self.assertEqual(student.school_id, school2.id)
+
+        await Student.filter(id=student1.id).update(school=school1)
+        schools = await School.all().order_by("students__name")
+        self.assertEqual([school.name for school in schools], ["School1", "School2"])
