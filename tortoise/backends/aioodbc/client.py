a55cc7126613d6f71c34577a84519e4473708cd0
reedjosh <reedjosh@oregonstate.edu>
2020-05-05 23:45:08 -0700

    Many fixes and db_create/db_delete work.

--- tortoise/backends/aioodbc/client.py
+++ tortoise/backends/aioodbc/client.py
@@ -1,10 +1,12 @@
 import asyncio
 from functools import wraps
 from typing import Any, Callable, List, Optional, SupportsInt, Tuple, TypeVar, Union
+import asyncio
 
 import aioodbc
 from pypika import Dialects
 from pypika.queries import Query, QueryBuilder
+
 
 import pyodbc
 from tortoise.backends.aioodbc.executor import AioodbcExecutor
@@ -40,12 +42,10 @@ def translate_exceptions(func: F) -> F:
         except pyodbc.IntegrityError as exc:
             raise IntegrityError(exc)
         except pyodbc.Error as exc:
-            if exc.args[0] == "HY000":
-                msg = list(exc.args[1])
-                msg = msg[: int(len(msg) / 2 - 7)]
-                msg = "".join(msg)  # type: ignore
-            raise OperationalError(msg)
-
+            if 'ORA-01400' in exc.args[1]:  # Inserting null on text field.
+                raise IntegrityError(exc)
+            raise OperationalError(exc)
+        
     return translate_exceptions_  # type: ignore
 
 
@@ -73,9 +73,8 @@ class OracleQuery(Query):  # type: ignore
 
 
 DSN_TEMPLATE = (
-    "ODBC;"
     "Driver={{{driver}}};"
-    "SERVER={c.host}:{c.port}"
+    "DBQ={c.host}:{c.port}"
     "/{c.database};"
     "UID={c.user};"
     "PWD={c.password}"
@@ -87,7 +86,7 @@ class AioodbcDBClient(BaseDBAsyncClient):
     query_class = OracleQuery
     executor_class = AioodbcExecutor
     schema_generator = AioodbcSchemaGenerator
-    capabilities = Capabilities("oracle")
+    capabilities = Capabilities("oracle", supports_transactions=False)
 
     def __init__(
         self, user: str, password: str, database: str, host: str, port: SupportsInt, **kwargs: Any,
@@ -128,20 +127,21 @@ class AioodbcDBClient(BaseDBAsyncClient):
             self._template["server_settings"] = {"search_path": self.schema}
 
         async def conn_attributes(conn):
-            # conn.setdecoding(pyodbc.SQL_CHAR, encoding='utf-8')
-            # conn.setdecoding(pyodbc.SQL_WCHAR, encoding='utf-8')
-            # conn.setdecoding(pyodbc.SQL_WMETADATA, encoding='utf-16le')
-            # conn.setencoding(encoding='utf-16')
             pass
 
         try:
             self._pool = await aioodbc.create_pool(
-                loop=None, **self._template, after_created=conn_attributes
+                loop=None, **self._template, after_created=conn_attributes, autocommit=True
             )
             self.log.debug("Created connection pool %s with params: %s", self._pool, self._template)
         except pyodbc.OperationalError:
             raise DBConnectionError(f"Can't establish connection to database {self.database}")
-        # Set post-connection variables
+        except pyodbc.Error as exc:
+            if 'ORA-01017' in exc.args[1]:
+                self.log.error("Could't connect with string %s", self._template)
+                raise DBConnectionError(f"Can't establish connection to database {self.database}")
+            else:
+                raise exc
 
     async def _expire_connections(self) -> None:
         if self._pool:  # pragma: nobranch
@@ -162,37 +162,57 @@ class AioodbcDBClient(BaseDBAsyncClient):
         self._template.clear()
 
     async def db_create(self) -> None:
-        query = """
-            BEGIN
-            FOR c IN (SELECT table_name FROM user_tables) LOOP
-            EXECUTE IMMEDIATE ('DROP TABLE "' || c.table_name || '" CASCADE CONSTRAINTS');
-            END LOOP;
-            END; """
-        await self.create_connection(with_db=True)
-        try:
-            await self.execute_script(query)
-        except pyodbc.ProgrammingError:  # pragma: nocoverage
-            pass
-        await self.close()
-        # await self.db_delete()
-        # await self.create_connection(with_db=False)
-        # await self.execute_script(f'CREATE DATABASE "{self.database}" OWNER "{self.user}"')
-        # await self.close()
+        conn_str = "Driver={OracleODBC-12c};DBQ=localhost:1539/XE;Uid=sys;Pwd=pass123 as sysdba"
+        conn = await aioodbc.connect(dsn=conn_str)
+        create = f"""CREATE PLUGGABLE DATABASE "{self.database}" 
+                                          ADMIN USER {self.user}
+                                         IDENTIFIED by {self.password};
+                                       """ 
+        open = f"""ALTER PLUGGABLE DATABASE "{self.database}" OPEN READ WRITE;""" 
+        change_container = f""" ALTER SESSION SET CONTAINER = "{self.database}" """
+
+        grant_perms  = f""" grant connect, resource to {self.user}; """
+        grant_space  = f""" grant unlimited tablespace to {self.user}; """
+
+        await conn.execute(create)
+        await conn.execute(open)
+        self.log.debug('Created database %s.', self.database)
+
+        await conn.execute(change_container)
+        self.log.debug('Changed to container %s.', self.database)
+
+        await conn.execute(grant_perms)
+        await conn.execute(grant_space)
+        self.log.debug('Granted permmissions to %s.', self.user)
+
+        await asyncio.sleep(.00001)
+        await conn.close()
 
     async def db_delete(self) -> None:
-        # query = """
-        #    BEGIN
-        #    FOR c IN (SELECT table_name FROM user_tables) LOOP
-        #    EXECUTE IMMEDIATE ('DROP TABLE "' || c.table_name || '" CASCADE CONSTRAINTS');
-        #    END LOOP;
-        #    END; """
-        # await self.create_connection(with_db=True)
-        # try:
-        #    await self.execute_script(query)
-        # except pyodbc.ProgrammingError:  # pragma: nocoverage
-        #    pass
-        # await self.close()
-        pass
+        conn_str = "Driver={OracleODBC-12c};DBQ=localhost:1539/XE;Uid=sys;Pwd=pass123 as sysdba"
+        conn = await aioodbc.connect(dsn=conn_str)
+        close = f""" ALTER PLUGGABLE DATABASE "{self.database}" close immediate; """
+        drop  = f""" DROP PLUGGABLE DATABASE "{self.database}" including datafiles; """
+        try:
+            await conn.execute(close)
+        except pyodbc.Error as exc:
+            if 'ORA-65020' in exc.args[1]:
+                self.log.warn("Database was already closed.")
+            elif 'ORA-65011' in exc.args[1]:
+                self.log.warn("Database didn't exist on close.")
+            else:
+                raise exc
+        self.log.debug('Starting delete.')
+        try:
+            await conn.execute(drop)
+        except pyodbc.Error as exc:
+            if 'ORA-65011' in exc.args[1]:
+                self.log.warn("Database didn'tn exist on delete.")
+            else:
+                raise exc
+        finally:
+            await asyncio.sleep(.00001)
+            await conn.close()
 
     def acquire_connection(self) -> Union["PoolConnectionWrapper", "ConnectionWrapper"]:
         return PoolConnectionWrapper(self._pool)
@@ -209,60 +229,62 @@ class AioodbcDBClient(BaseDBAsyncClient):
         # and store it in the table. From there have the OracleQueryBuilder build inserts with
         # currval already requested.
 
+        print('#'*15, 'exe insert', '#'*15)
+        print(query, values)
+        print('#'*30)
+
         # Parse table from query. (INSERT INTO "TABLE"...)
         table = query.split()[2].replace('"', "'")
         data_default_query = f"""
             SELECT DATA_DEFAULT FROM USER_TAB_COLUMNS
             WHERE TABLE_NAME = {table} AND DATA_DEFAULT IS NOT NULL"""
+        
+        pk_query = f""" SELECT cols.column_name
+            FROM all_constraints cons, all_cons_columns cols
+            WHERE cols.table_name = {table}
+            AND cons.constraint_type = 'P'
+            AND cons.constraint_name = cols.constraint_name
+            AND cons.owner = cols.owner """
         async with self.acquire_connection() as connection:
             self.log.debug("%s: %s", query, values)
-
             async with connection.cursor() as crsr:
-                # Get sequence pointer for table.
-                await crsr.execute(data_default_query)
-                res = (await crsr.fetchone())[0]
-                seq_pointer = res.split(".")[1]
-
                 # Actual insertion.
                 params = [item for item in [query, values] if item]
                 await crsr.execute(*params)
 
-                # Get current value of sequence post insertion.
+                # Get sequence pointer for table.
+                await crsr.execute(data_default_query)
+                res = (await crsr.fetchone())
+                if res:
+                    res = res[0]
+                    seq_pointer = res.split(".")[1]
+                    # Get current value of sequence post insertion.
+                    currval_query = f"SELECT {seq_pointer}.currval FROM dual;"
+                    await crsr.execute(currval_query)
+                    ret_val = (await crsr.fetchone())[0]
+                    return ret_val
+                return 0
+                # Here be need for PK finding.
+                await crsr.execute(pk_query)
+                pk = (await crsr.fetchone())[0]
                 currval_query = f"SELECT {seq_pointer}.currval FROM dual;"
-                await crsr.execute(currval_query)
-                return (await crsr.fetchone())[0]
 
-    #    @translate_exceptions
-    #    async def execute_many(self, query: str, values: list) -> None:
-    #        async with self.acquire_connection() as cnxn:
-    #            autocommit_orig = cnxn.autocommit
-    #            cnxn.autocommit = False
-    #            async with cnxn.cursor() as crsr:
-    #                self.log.debug("%s: %s", query, values)
-    #                try:
-    #                    # Possibly useful for speedups.
-    #                    # crsr.fast_executemany = True
-    #                    await crsr.executemany(query, values)
-    #                except pyodbc.DatabaseError:
-    #                    cnxn.rollback()
-    #                else:
-    #                    cnxn.commit()
-    #                finally:
-    #                    cnxn.autocommit = autocommit_orig
 
     @translate_exceptions
     async def execute_query(
         self, query: str, values: Optional[list] = None
     ) -> Tuple[int, List[dict]]:
-        print("#" * 30)
-        print(query)
-        print("#" * 30)
         async with self.acquire_connection() as connection:
             self.log.debug("%s: %s", query, values)
             async with connection.cursor() as crsr:
                 params = [item for item in [query, values] if item]
+                print('#'*15, 'exe query', '#'*15)
+                print(query)
+                print('#'*30)
                 await crsr.execute(*params)
                 if "UPDATE" in query:
+                    return 1, []
+                if "DELETE" in query:
                     return 1, []
                 rows = await crsr.fetchall()
                 if rows:
@@ -279,11 +301,11 @@ class AioodbcDBClient(BaseDBAsyncClient):
         # queries = [query for query in queries if query]
         # for query in queries:
         #    query = query + ';'
-        print("#" * 30)
-        print(query)
-        print("#" * 30)
         async with self.acquire_connection() as connection:
             async with connection.cursor() as crsr:
+                #print('#'*15, 'exe script', '#'*15)
+                #print(query)
+                #print('#'*30)
                 self.log.debug(query)
                 await crsr.execute(query)
 
@@ -297,7 +319,6 @@ class TransactionWrapper(AioodbcDBClient, BaseTransactionWrapper):
         self.log = connection.log
         self._finalized: Optional[bool] = None
         self._parent = connection
-        self._autocommit_orig = None
 
     def _in_transaction(self) -> "TransactionContext":
         return NestedTransactionPooledContext(self)
@@ -322,7 +343,6 @@ class TransactionWrapper(AioodbcDBClient, BaseTransactionWrapper):
         if self._finalized:
             raise TransactionManagementError("Transaction already finalised")
         await self._connection.commit()
-        self._connection.autocommit = self._autocommit_orig
         self._finalized = True
 
     async def rollback(self) -> None:
