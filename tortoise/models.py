1d7c80880e213f72d5d9c1a8a5f55474716fdf37
evstrat <evstrat.bg@mail.ru>
2020-09-13 20:16:58 +0300

    fix misstypo

--- tortoise/models.py
+++ tortoise/models.py
@@ -138,7 +138,7 @@ def _get_comments(cls: "Type[Model]") -> Dict[str, str]:
     """
     try:
         source = inspect.getsource(cls)
-    except (TypeError, OsError):  # pragma: nocoverage
+    except (TypeError, OSError):  # pragma: nocoverage
         return {}
     comments = {}
 
