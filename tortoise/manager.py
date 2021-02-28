82e7fc02f9537e69ae07af58e5072aed8dddbf5c
long2ice <long2ice@gmail.com>
2021-02-28 15:29:57 +0800

    Fix type

--- tortoise/manager.py
+++ tortoise/manager.py
@@ -1,11 +1,13 @@
-from tortoise.queryset import QuerySet
+from typing import Type
+
+from tortoise.queryset import MODEL, QuerySet
 
 
 class Manager:
-    def __init__(self):
-        self._model = None
+    def __init__(self) -> None:
+        self._model: Type[MODEL] = None  # type:ignore
 
-    def get_queryset(self):
+    def get_queryset(self) -> QuerySet:
         return QuerySet(self._model)
 
     def __getattr__(self, item):
