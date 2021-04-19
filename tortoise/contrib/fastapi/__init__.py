0ff7cf1e7b8e13ed274e2b7d89392e138cbbaa9b
Ivan Dmitrievsky <ivan.dmitrievsky@gmail.com>
2021-04-19 20:46:53 +0300

    Avoid using root logger
    This relates to #529 and implements a suggested fix.

--- tortoise/contrib/fastapi/__init__.py
+++ tortoise/contrib/fastapi/__init__.py
@@ -9,6 +9,7 @@ from pydantic import BaseModel  # pylint: disable=E0611
 from tortoise import Tortoise
 from tortoise.exceptions import DoesNotExist, IntegrityError
 
+logger = logging.getLogger("tortoise-fastapi")
 
 class HTTPNotFoundError(BaseModel):
     detail: str
@@ -91,15 +92,15 @@ def register_tortoise(
     @app.on_event("startup")
     async def init_orm() -> None:  # pylint: disable=W0612
         await Tortoise.init(config=config, config_file=config_file, db_url=db_url, modules=modules)
-        logging.info("Tortoise-ORM started, %s, %s", Tortoise._connections, Tortoise.apps)
+        logger.info("Tortoise-ORM started, %s, %s", Tortoise._connections, Tortoise.apps)
         if generate_schemas:
-            logging.info("Tortoise-ORM generating schema")
+            logger.info("Tortoise-ORM generating schema")
             await Tortoise.generate_schemas()
 
     @app.on_event("shutdown")
     async def close_orm() -> None:  # pylint: disable=W0612
         await Tortoise.close_connections()
-        logging.info("Tortoise-ORM shutdown")
+        logger.info("Tortoise-ORM shutdown")
 
     if add_exception_handlers:
 
