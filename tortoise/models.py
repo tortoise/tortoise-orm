b22e80b437126ac9cc84e5c408a3ad1240a96d1f
Chise1 <chise123@live.com>
2023-02-18 17:53:13 +0800

    optimize Model printint

--- tortoise/models.py
+++ tortoise/models.py
@@ -749,6 +749,8 @@ class Model(metaclass=ModelMeta):
         return self
 
     def __str__(self) -> str:
+        if self.pk:
+            return f"<{self.__class__.__name__}: {self.pk}>"
         return f"<{self.__class__.__name__}>"
 
     def __repr__(self) -> str:
