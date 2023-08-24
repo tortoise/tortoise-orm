09329c4093d6af8cdb94b847dea3d380ff306b55
Jorge Massih <jorgmassih@gmail.com>
2023-08-24 09:36:11 -0400

    chore: bypass linter false-positive

--- tortoise/contrib/mysql/fields.py
+++ tortoise/contrib/mysql/fields.py
@@ -1,11 +1,17 @@
-from typing import TYPE_CHECKING, Any, Optional, Type, Union
+from typing import (  # noqa pylint: disable=unused-import
+    TYPE_CHECKING,
+    Any,
+    Optional,
+    Type,
+    Union,
+)
 from uuid import UUID, uuid4
 
 from tortoise.fields import Field
 from tortoise.fields import UUIDField as UUIDFieldBase
 
 if TYPE_CHECKING:  # pragma: nocoverage
-    from tortoise.models import Model
+    from tortoise.models import Model  # noqa pylint: disable=unused-import
 
 
 class GeometryField(Field):
@@ -20,7 +26,7 @@ class UUIDField(UUIDFieldBase):
 
     If used as a primary key, it will auto-generate a UUID4 by default.
 
-    ``binary_compression``: (bool)
+    ``binary_compression`` (bool):
         If True, the UUID will be stored in binary format.
         This will save 6 bytes per UUID in the database.
         Note: that this is a MySQL-only feature.
@@ -51,7 +57,7 @@ class UUIDField(UUIDFieldBase):
     def to_python_value(self, value: Any) -> Optional[UUID]:
         if value is None or isinstance(value, UUID):
             return value
-        elif self._binary_compression:
+        elif self._binary_compression and isinstance(value, bytes):
             return UUID(bytes=value)
         else:
             return UUID(value)
