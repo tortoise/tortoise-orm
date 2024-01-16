6ac304dbeb893fcb49303c2daa96a70d10fba129
WizzyGeek <ojasscoding@gmail.com>
2024-01-17 01:41:59 +0530

    misc: re-export symbols in tortoise.contrib.pydantic

--- tortoise/contrib/pydantic/__init__.py
+++ tortoise/contrib/pydantic/__init__.py
@@ -1,4 +1,12 @@
-from tortoise.contrib.pydantic.base import PydanticListModel  # noqa
-from tortoise.contrib.pydantic.base import PydanticModel  # noqa
-from tortoise.contrib.pydantic.creator import pydantic_model_creator  # noqa
-from tortoise.contrib.pydantic.creator import pydantic_queryset_creator  # noqa
+from tortoise.contrib.pydantic.base import PydanticListModel, PydanticModel
+from tortoise.contrib.pydantic.creator import (
+    pydantic_model_creator,
+    pydantic_queryset_creator,
+)
+
+__all__ = (
+    "PydanticListModel",
+    "PydanticModel",
+    "pydantic_model_creator",
+    "pydantic_queryset_creator",
+)
