7ded5c7cdbdcdc56d5def36c8cf865abc27c9823
Waket Zheng <waketzheng@gmail.com>
2024-04-27 14:03:06 +0800

    Enhancement for FastAPI lifespan support (#1371)(#1576) (#1541)

--- tortoise/contrib/fastapi/__init__.py
+++ tortoise/contrib/fastapi/__init__.py
@@ -1,22 +1,148 @@
-from types import ModuleType
-from typing import Dict, Iterable, Optional, Union
+from __future__ import annotations
 
-from fastapi import FastAPI
+import warnings
+from contextlib import AbstractAsyncContextManager, asynccontextmanager
+from types import ModuleType
+from typing import TYPE_CHECKING, Dict, Iterable, Optional, Union
+
+from fastapi.responses import JSONResponse
 from pydantic import BaseModel  # pylint: disable=E0611
-from starlette.requests import Request
-from starlette.responses import JSONResponse
+from starlette.routing import _DefaultLifespan
 
 from tortoise import Tortoise, connections
 from tortoise.exceptions import DoesNotExist, IntegrityError
 from tortoise.log import logger
+
+if TYPE_CHECKING:
+    from fastapi import FastAPI, Request
 
 
 class HTTPNotFoundError(BaseModel):
     detail: str
 
 
+class RegisterTortoise(AbstractAsyncContextManager):
+    """
+    Registers Tortoise-ORM with set-up and tear-down
+    inside a FastAPI application's lifespan.
+
+    You can configure using only one of ``config``, ``config_file``
+    and ``(db_url, modules)``.
+
+    Parameters
+    ----------
+    app:
+        FastAPI app.
+    config:
+        Dict containing config:
+
+        Example
+        -------
+
+        .. code-block:: python3
+
+            {
+                'connections': {
+                    # Dict format for connection
+                    'default': {
+                        'engine': 'tortoise.backends.asyncpg',
+                        'credentials': {
+                            'host': 'localhost',
+                            'port': '5432',
+                            'user': 'tortoise',
+                            'password': 'qwerty123',
+                            'database': 'test',
+                        }
+                    },
+                    # Using a DB_URL string
+                    'default': 'postgres://postgres:qwerty123@localhost:5432/events'
+                },
+                'apps': {
+                    'models': {
+                        'models': ['__main__'],
+                        # If no default_connection specified, defaults to 'default'
+                        'default_connection': 'default',
+                    }
+                }
+            }
+
+    config_file:
+        Path to .json or .yml (if PyYAML installed) file containing config with
+        same format as above.
+    db_url:
+        Use a DB_URL string. See :ref:`db_url`
+    modules:
+        Dictionary of ``key``: [``list_of_modules``] that defined "apps" and modules that
+        should be discovered for models.
+    generate_schemas:
+        True to generate schema immediately. Only useful for dev environments
+        or SQLite ``:memory:`` databases
+    add_exception_handlers:
+        True to add some automatic exception handlers for ``DoesNotExist`` & ``IntegrityError``.
+        This is not recommended for production systems as it may leak data.
+
+    Raises
+    ------
+    ConfigurationError
+        For any configuration error
+    """
+
+    def __init__(
+        self,
+        app: FastAPI,
+        config: Optional[dict] = None,
+        config_file: Optional[str] = None,
+        db_url: Optional[str] = None,
+        modules: Optional[Dict[str, Iterable[Union[str, ModuleType]]]] = None,
+        generate_schemas: bool = False,
+        add_exception_handlers: bool = False,
+    ) -> None:
+        self.app = app
+        self.config = config
+        self.config_file = config_file
+        self.db_url = db_url
+        self.modules = modules
+        self.generate_schemas = generate_schemas
+        if add_exception_handlers:
+
+            @app.exception_handler(DoesNotExist)
+            async def doesnotexist_exception_handler(request: "Request", exc: DoesNotExist):
+                return JSONResponse(status_code=404, content={"detail": str(exc)})
+
+            @app.exception_handler(IntegrityError)
+            async def integrityerror_exception_handler(request: "Request", exc: IntegrityError):
+                return JSONResponse(
+                    status_code=422,
+                    content={"detail": [{"loc": [], "msg": str(exc), "type": "IntegrityError"}]},
+                )
+
+    async def init_orm(self) -> None:  # pylint: disable=W0612
+        config, config_file = self.config, self.config_file
+        db_url, modules = self.db_url, self.modules
+        await Tortoise.init(config=config, config_file=config_file, db_url=db_url, modules=modules)
+        logger.info("Tortoise-ORM started, %s, %s", connections._get_storage(), Tortoise.apps)
+        if self.generate_schemas:
+            logger.info("Tortoise-ORM generating schema")
+            await Tortoise.generate_schemas()
+
+    @staticmethod
+    async def close_orm() -> None:  # pylint: disable=W0612
+        await connections.close_all()
+        logger.info("Tortoise-ORM shutdown")
+
+    def __call__(self, *args, **kwargs) -> "RegisterTortoise":
+        return self
+
+    async def __aenter__(self) -> "RegisterTortoise":
+        await self.init_orm()
+        return self
+
+    async def __aexit__(self, *args, **kw):
+        await self.close_orm()
+
+
 def register_tortoise(
-    app: FastAPI,
+    app: "FastAPI",
     config: Optional[dict] = None,
     config_file: Optional[str] = None,
     db_url: Optional[str] = None,
@@ -88,29 +214,40 @@ def register_tortoise(
     ConfigurationError
         For any configuration error
     """
+    orm = RegisterTortoise(
+        app,
+        config,
+        config_file,
+        db_url,
+        modules,
+        generate_schemas,
+        add_exception_handlers,
+    )
+    if isinstance(lifespan := app.router.lifespan_context, _DefaultLifespan):
+        # Leave on_event here to compare with old versions
+        # So people can upgrade tortoise-orm in running project without changing any code
 
-    @app.on_event("startup")
-    async def init_orm() -> None:  # pylint: disable=W0612
-        await Tortoise.init(config=config, config_file=config_file, db_url=db_url, modules=modules)
-        logger.info("Tortoise-ORM started, %s, %s", connections._get_storage(), Tortoise.apps)
-        if generate_schemas:
-            logger.info("Tortoise-ORM generating schema")
-            await Tortoise.generate_schemas()
+        @app.on_event("startup")  # type: ignore[unreachable]
+        async def init_orm() -> None:  # pylint: disable=W0612
+            await orm.init_orm()
 
-    @app.on_event("shutdown")
-    async def close_orm() -> None:  # pylint: disable=W0612
-        await connections.close_all()
-        logger.info("Tortoise-ORM shutdown")
+        @app.on_event("shutdown")
+        async def close_orm() -> None:  # pylint: disable=W0612
+            await orm.close_orm()
 
-    if add_exception_handlers:
+    else:
+        # If custom lifespan was passed to app, register tortoise in it
+        warnings.warn(
+            "`register_tortoise` function is deprecated, "
+            "use the `RegisterTortoise` class instead."
+            "See more about it on https://tortoise.github.io/examples/fastapi",
+            DeprecationWarning,
+        )
 
-        @app.exception_handler(DoesNotExist)
-        async def doesnotexist_exception_handler(request: Request, exc: DoesNotExist):
-            return JSONResponse(status_code=404, content={"detail": str(exc)})
+        @asynccontextmanager
+        async def orm_lifespan(app_instance: "FastAPI"):
+            async with orm:
+                async with lifespan(app_instance):
+                    yield
 
-        @app.exception_handler(IntegrityError)
-        async def integrityerror_exception_handler(request: Request, exc: IntegrityError):
-            return JSONResponse(
-                status_code=422,
-                content={"detail": [{"loc": [], "msg": str(exc), "type": "IntegrityError"}]},
-            )
+        app.router.lifespan_context = orm_lifespan
