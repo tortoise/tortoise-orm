e2ac054d33c77542866c352d5819840847060328
Waket Zheng <waketzheng@gmail.com>
2024-05-10 09:59:36 +0800

    fix missing assert

--- tests/test_version.py
+++ tests/test_version.py
@@ -5,4 +5,4 @@ from tortoise import __version__
 
 def test_version():
     r = subprocess.run(["poetry", "version", "-s"], capture_output=True)
-    r.stdout.decode() == __version__
+    assert r.stdout.decode().strip() == __version__
