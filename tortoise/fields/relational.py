055d19dc080c2594722ecf7ccec1ce6d58459527
Waket Zheng <waketzheng@gmail.com>
2024-05-21 23:53:40 +0800

    refactor: optimize m2m add logic

--- tortoise/fields/relational.py
+++ tortoise/fields/relational.py
@@ -96,7 +96,6 @@ class ReverseRelation(Generic[MODEL]):
     async def __aiter__(self) -> AsyncGenerator[Any, MODEL]:
         if not self._fetched:
             self._set_result_for_query(await self)
-
         for val in self.related_objects:
             yield val
 
@@ -165,56 +164,35 @@ class ManyToManyRelation(ReverseRelation[MODEL]):
             return
         if not self.instance._saved_in_db:
             raise OperationalError(f"You should first call .save() on {self.instance}")
-        db = using_db if using_db else self.remote_model._meta.db
+        db = using_db or self.remote_model._meta.db
         pk_formatting_func = type(self.instance)._meta.pk.to_db_value
         related_pk_formatting_func = type(instances[0])._meta.pk.to_db_value
-        through_table = Table(self.field.through)
-        select_query = (
-            db.query_class.from_(through_table)
-            .where(
-                through_table[self.field.backward_key]
-                == pk_formatting_func(self.instance.pk, self.instance)
-            )
-            .select(self.field.backward_key, self.field.forward_key)
-        )
-        query = db.query_class.into(through_table).columns(
-            through_table[self.field.forward_key],
-            through_table[self.field.backward_key],
-        )
-
-        if len(instances) == 1:
-            criterion = through_table[self.field.forward_key] == related_pk_formatting_func(
-                instances[0].pk, instances[0]
-            )
-        else:
-            criterion = through_table[self.field.forward_key].isin(
-                [related_pk_formatting_func(i.pk, i) for i in instances]
-            )
-
-        select_query = select_query.where(criterion)
-
-        # TODO: This is highly inefficient. Should use UNIQUE index by default.
-        #  And optionally allow duplicates.
-        _, already_existing_relations_raw = await db.execute_query(str(select_query))
-        already_existing_relations = {
-            (
-                pk_formatting_func(r[self.field.backward_key], self.instance),
-                related_pk_formatting_func(r[self.field.forward_key], self.instance),
-            )
-            for r in already_existing_relations_raw
-        }
-
-        insert_is_required = False
+        pk_b = pk_formatting_func(self.instance.pk, self.instance)
+        pks_f: list = []
         for instance_to_add in instances:
             if not instance_to_add._saved_in_db:
                 raise OperationalError(f"You should first call .save() on {instance_to_add}")
             pk_f = related_pk_formatting_func(instance_to_add.pk, instance_to_add)
-            pk_b = pk_formatting_func(self.instance.pk, self.instance)
-            if (pk_b, pk_f) in already_existing_relations:
-                continue
-            query = query.insert(pk_f, pk_b)
-            insert_is_required = True
-        if insert_is_required:
+            pks_f.append(pk_f)
+        through_table = Table(self.field.through)
+        backward_key, forward_key = self.field.backward_key, self.field.forward_key
+        backward_field, forward_field = through_table[backward_key], through_table[forward_key]
+        select_query = (
+            db.query_class.from_(through_table).where(backward_field == pk_b).select(forward_key)
+        )
+        criterion = forward_field == pks_f[0] if len(pks_f) == 1 else forward_field.isin(pks_f)
+        select_query = select_query.where(criterion)
+
+        _, already_existing_relations_raw = await db.execute_query(str(select_query))
+        already_existing_forward_pks = {
+            related_pk_formatting_func(r[forward_key], self.instance)
+            for r in already_existing_relations_raw
+        }
+
+        if pks_f_to_insert := set(pks_f) - already_existing_forward_pks:
+            query = db.query_class.into(through_table).columns(forward_field, backward_field)
+            for pk_f in pks_f_to_insert:
+                query = query.insert(pk_f, pk_b)
             await db.execute_query(str(query))
 
     async def clear(self, using_db: "Optional[BaseDBAsyncClient]" = None) -> None:
