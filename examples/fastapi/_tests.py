ffaa45d6119fe1ff7a4487e062371a932dbb8e13
Abeautifulsnow <1491444340@qq.com>
2024-06-14 10:53:19 +0800

    Update _tests.py

--- examples/fastapi/_tests.py
+++ examples/fastapi/_tests.py
@@ -1,11 +1,12 @@
 # mypy: no-disallow-untyped-decorators
 # pylint: disable=E0611,E0401
+import datetime
 from typing import AsyncGenerator
 
 import pytest
 from asgi_lifespan import LifespanManager
 from httpx import ASGITransport, AsyncClient
-from main import app
+from main import app, app_east
 from models import Users
 
 
@@ -33,3 +34,32 @@ async def test_create_user(client: AsyncClient) -> None:  # nosec
 
     user_obj = await Users.get(id=user_id)
     assert user_obj.id == user_id
+
+    # UTC timezone.
+    created_at = user_obj.created_at
+    assert created_at.hour - datetime.datetime.now().hour == -8
+
+
+@pytest.fixture(scope="module")
+async def client_east() -> AsyncGenerator[AsyncClient, None]:
+    async with LifespanManager(app_east):
+        transport = ASGITransport(app=app_east)
+        async with AsyncClient(transport=transport, base_url="http://test") as c:
+            yield c
+
+
+@pytest.mark.anyio
+async def test_create_user_east(client_east: AsyncClient) -> None:  # nosec
+    response = await client_east.post("/users_east", json={"username": "admin"})
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["username"] == "admin"
+    assert "id" in data
+    user_id = data["id"]
+
+    user_obj = await Users.get(id=user_id)
+    assert user_obj.id == user_id
+
+    # Verify that the time zone is East 8.
+    created_at = user_obj.created_at
+    assert created_at.hour - datetime.datetime.now().hour == 0
