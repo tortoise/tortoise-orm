d6ff0f9d1356d408fbc70f4efa1c157219e33662
Waket Zheng <waketzheng@gmail.com>
2024-06-18 16:28:31 +0800

    fixing codacy issues

--- tests/contrib/test_decorator.py
+++ tests/contrib/test_decorator.py
@@ -1,6 +1,4 @@
 import subprocess
-import sys
-
 
 from tortoise.contrib import test
 
@@ -8,7 +6,7 @@ from tortoise.contrib import test
 class TestDecorator(test.TestCase):
     @test.requireCapability(dialect="sqlite")
     async def test_init_memory_sqlite(self):
-        r = subprocess.run([sys.executable, "examples/basic.py"], capture_output=True)
+        r = subprocess.run(["python", "examples/basic.py"], capture_output=True)
         output = r.stdout.decode()
         s = "[{'id': 1, 'name': 'Updated name'}, {'id': 2, 'name': 'Test 2'}]"
         self.assertIn(s, output)
