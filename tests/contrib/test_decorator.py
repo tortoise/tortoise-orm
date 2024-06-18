e347d55b170772178be6aca0979a0287f61b6543
Waket Zheng <waketzheng@gmail.com>
2024-06-18 15:34:32 +0800

    fixing codacy issue

--- tests/contrib/test_decorator.py
+++ tests/contrib/test_decorator.py
@@ -1,5 +1,4 @@
 import subprocess
-import shlex
 import sys
 
 
@@ -9,8 +8,7 @@ from tortoise.contrib import test
 class TestDecorator(test.TestCase):
     @test.requireCapability(dialect="sqlite")
     async def test_init_memory_sqlite(self):
-        cmd = f"{sys.executable} examples/basic.py"
-        r = subprocess.run(shlex.split(cmd), capture_output=True)
+        r = subprocess.run([sys.executable, "examples/basic.py"], capture_output=True)
         output = r.stdout.decode()
         s = "[{'id': 1, 'name': 'Updated name'}, {'id': 2, 'name': 'Test 2'}]"
         self.assertIn(s, output)
