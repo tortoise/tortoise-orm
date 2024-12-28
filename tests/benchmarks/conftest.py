e4602398e6164b57b8ca262850602f41cad99a56
henadzit <henadzi.tsaryk@gmail.com>
2024-12-28 21:19:50 +0100

    Skip benchmarks during regular tests

--- tests/benchmarks/conftest.py
+++ tests/benchmarks/conftest.py
@@ -6,3 +6,9 @@ from tortoise.contrib.test import _restore_default
 @pytest.fixture(scope="module", autouse=True)
 def setup_database():
     _restore_default()
+
+
+@pytest.fixture(scope="module", autouse=True)
+def skip_if_codspeed_not_enabled(request):
+    if not request.config.getoption("--codspeed"):
+        pytest.skip("codspeed tests are disabled")
