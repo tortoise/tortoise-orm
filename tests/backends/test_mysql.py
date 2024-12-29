eeb3ddbe3eecf243715d2926dcf43365e7fffee7
henadzit <henadzi.tsaryk@gmail.com>
2024-12-29 20:52:05 +0100

    Set read_timeout just for asyncmy

--- tests/backends/test_mysql.py
+++ tests/backends/test_mysql.py
@@ -28,8 +28,14 @@ class TestMySQL(test.SimpleTestCase):
 
     async def test_ssl_true(self):
         self.db_config["connections"]["models"]["credentials"]["ssl"] = True
-        # setting read_timeout for asyncmy. Otherwise, it will hang forever.
-        self.db_config["connections"]["models"]["credentials"]["read_timeout"] = 1
+        try:
+            import asyncmy  # noqa pylint: disable=unused-import
+
+            # setting read_timeout for asyncmy. Otherwise, it will hang forever.
+            self.db_config["connections"]["models"]["credentials"]["read_timeout"] = 1
+        except ImportError:
+            pass
+
         with self.assertRaises(ConnectionError):
             await Tortoise.init(self.db_config, _create_db=True)
 
