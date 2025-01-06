ec16e64484628fdc5c6790179b8c0684ea09e76a
eyllanesc-JE <eyllanesc@gmail.com>
2025-01-06 01:21:47 -0500

    Add localns to get_annotations

--- tortoise/contrib/pydantic/utils.py
+++ tortoise/contrib/pydantic/utils.py
@@ -12,4 +12,9 @@ def get_annotations(cls: "Type[Model]", method: Optional[Callable] = None) -> Di
     :param method: If specified, we try to get the annotations for the callable
     :return: The list of annotations
     """
-    return typing.get_type_hints(method or cls)
+    localns = (
+        tortoise.Tortoise.apps.get(cls._meta.app, None)
+        if cls._meta.app
+        else None
+    )
+    return typing.get_type_hints(method or cls, localns=localns)
