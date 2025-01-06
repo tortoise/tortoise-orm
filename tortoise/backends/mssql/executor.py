f5801c7b1787bbf5d8afe9f909974cef8079b4b8
henadzit <henadzi.tsaryk@gmail.com>
2025-01-06 10:55:46 +0100

    Remove unused code from mssql executor

--- tortoise/backends/mssql/executor.py
+++ tortoise/backends/mssql/executor.py
@@ -1,18 +1,7 @@
-from typing import Any, Optional, Type, Union
+from typing import Any
 
-from tortoise import Model
 from tortoise.backends.odbc.executor import ODBCExecutor
 from tortoise.exceptions import UnSupportedError
-from tortoise.fields import BooleanField
-
-
-def to_db_bool(
-    self: BooleanField, value: Optional[Union[bool, int]], instance: Union[Type[Model], Model]
-) -> Optional[int]:
-    self.validate(value)
-    if value is None:
-        return None
-    return int(bool(value))
 
 
 class MSSQLExecutor(ODBCExecutor):
