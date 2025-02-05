37891a99ccbf343a6062d074dd8cbf90ec01f8ac
alistairmaclean <34235348+alistairmaclean@users.noreply.github.com>
2025-02-05 17:10:29 +0100

    Add logic to handle `globalns` and `localns` in `pydantic_model_creator`

--- tortoise/contrib/pydantic/utils.py
+++ tortoise/contrib/pydantic/utils.py
@@ -5,11 +5,13 @@ if TYPE_CHECKING:  # pragma: nocoverage
     from tortoise.models import Model
 
 
-def get_annotations(cls: "type[Model]", method: Optional[Callable] = None) -> dict[str, Any]:
+def get_annotations(cls: "type[Model]", method: Optional[Callable] = None, globalns: dict = None, localns: dict = None) -> dict[str, Any]:
     """
     Get all annotations including base classes
     :param cls: The model class we need annotations from
     :param method: If specified, we try to get the annotations for the callable
+    :param globalns: If specified, use this dictionary as the globals map
+    :param localns: If specified, use this dictionary as the locals map
     :return: The list of annotations
     """
-    return get_type_hints(method or cls)
+    return get_type_hints(method or cls, globalns, localns)
