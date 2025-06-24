57aa615c65e0eb2a7725219f47e3f84ba029216a
Sietros <33125382+sietr0s@users.noreply.github.com>
2025-06-24 18:29:09 +0300

    Added m2m related table alias

--- tortoise/query_utils.py
+++ tortoise/query_utils.py
@@ -37,6 +37,7 @@ def get_joins_for_related_field(
     related_table: Table = related_field.related_model._meta.basetable
     if isinstance(related_field, ManyToManyFieldInstance):
         through_table = Table(related_field.through)
+        related_table = related_table.as_(f"{table.get_table_name()}__{related_field_name}")
         required_joins.append(
             (
                 through_table,
