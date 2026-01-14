010506bf0b220fe8d4dbf0339cf1078642abaa54
Waket Zheng <waketzheng@gmail.com>
2026-01-14 18:24:55 +0800

    tests: add invalid cases

--- tests/utils/test_parse_datetime.py
+++ tests/utils/test_parse_datetime.py
@@ -1,5 +1,7 @@
 from unittest import skipIf
 
+import pytest
+
 from tortoise.contrib import test
 from tortoise.utils import parse_datetime
 
@@ -13,12 +15,22 @@ except ImportError:
 class TestParseDatetime(test.TestCase):
     def test_no_timezone(self):
         cases = [
+            "20260101",
             "2026-01-13",
+            "20260113 01",
+            "20260113 0102",
+            "20260113 010203",
             "2026-01-14 00:00:01",
+            "2026-01-15 01:23:45.12345",
             "2026-01-15 01:23:45.123456",
         ]
         for time_str in cases:
             assert ciso8601.parse_datetime(time_str) == parse_datetime(time_str)
+        for invalid in ["2026-00-00", "26-01-02"]:
+            with pytest.raises(ValueError):
+                ciso8601.parse_datetime(invalid)
+            with pytest.raises(ValueError):
+                parse_datetime(invalid)
 
     def test_with_timezone(self):
         cases = [
@@ -29,3 +41,9 @@ class TestParseDatetime(test.TestCase):
         ]
         for time_str in cases:
             assert ciso8601.parse_datetime(time_str) == parse_datetime(time_str)
+        for invalid_zone in ["+00:001", "+25:00"]:
+            invalid = "2026-01-14T00:00:00" + invalid_zone
+            with pytest.raises(ValueError):
+                ciso8601.parse_datetime(invalid)
+            with pytest.raises(ValueError):
+                parse_datetime(invalid)
