6b6aa17d70c9137685ea044e1cbf3024df6b89b6
RuslanUC <dev_ruslan_uc@protonmail.com>
2026-01-20 21:42:03 +0200

    remove .order_by from subquery in test_queryset.py

--- tests/test_queryset.py
+++ tests/test_queryset.py
@@ -862,19 +862,16 @@ class TestQueryset(test.TestCase):
         await Book.create(name="2", author=author2, rating=3)
         await Book.create(name="3", author=author3, rating=5)
 
-        subquery = (
-            Author.annotate(rating=Avg("books__rating"))
-            .filter(rating__gte=3)
-            .order_by("id")
-            .values_list("id", flat=True)
-        )
+        subquery = Author.annotate(rating=Avg("books__rating")).filter(rating__gte=3)
 
-        subquery_ret = await subquery
+        subquery_ret = await subquery.order_by("id").values_list("id", flat=True)
         self.assertEqual(len(subquery_ret), 2)
         self.assertEqual(subquery_ret[0], author2.pk)
         self.assertEqual(subquery_ret[1], author3.pk)
 
-        ret = await Author.filter(id__in=Subquery(subquery)).order_by("id")
+        ret = await Author.filter(id__in=Subquery(subquery.values_list("id", flat=True))).order_by(
+            "id"
+        )
         self.assertEqual(ret[0], author2)
         self.assertEqual(ret[1], author3)
 
