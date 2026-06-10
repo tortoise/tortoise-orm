27c291330222f9b6654c1b64525b2b6153b594d5
Waket Zheng <waketzheng@gmail.com>
2026-06-10 10:16:04 +0800

    Add dedicated ISO8601 fallback tests

--- tests/fields/test_time.py
+++ tests/fields/test_time.py
@@ -10,7 +10,6 @@ import pytest
 
 from tests import testmodels
 from tortoise import fields, timezone
-from tortoise._iso8601 import parse_datetime as parse_datetime_fallback
 from tortoise.contrib import test
 from tortoise.contrib.test.condition import NotIn
 from tortoise.exceptions import ConfigurationError, IntegrityError
@@ -756,47 +755,6 @@ def test_zoneinfo():
         parse_timezone("invalid-zone-name")
     with pytest.raises(ZoneInfoNotFoundError):
         parse_timezone("Invalid/Zonename")
-
-
-def test_parse_datetime_fallback():
-    assert parse_datetime_fallback("2020-08-17") == datetime(2020, 8, 17)
-    assert parse_datetime_fallback("2020-08-17T00:00:00Z") == datetime(
-        2020, 8, 17, tzinfo=UTC
-    )
-    assert parse_datetime_fallback("2020-230T12:34:56Z") == datetime(
-        2020, 8, 17, 12, 34, 56, tzinfo=UTC
-    )
-    assert parse_datetime_fallback("2020-230t12:34:56") == datetime(2020, 8, 17, 12, 34, 56)
-    assert parse_datetime_fallback("2020230T123456") == datetime(2020, 8, 17, 12, 34, 56)
-
-
-@pytest.mark.parametrize(
-    "value",
-    [
-        "2020-08-17",
-        "20200817",
-        "2020-08-17T12:34:56",
-        "2020-08-17t12:34:56",
-        "2020-08-17 12:34:56",
-        "20200817T123456",
-        "2020-08-17T12:34:56Z",
-        "2020-08-17T12:34:56z",
-        "2020-08-17T12:34:56+08",
-        "2020-08-17T12:34:56+0800",
-        "2020-08-17T12:34:56+08:00",
-        "2020-W34-1",
-        "2020W341",
-        "2020-230",
-        "2020230",
-        "2020-230T12:34:56",
-        "2020-230t12:34:56",
-        "2020230T123456",
-    ],
-)
-def test_parse_datetime_fallback_matches_ciso8601(value):
-    ciso8601 = pytest.importorskip("ciso8601")
-
-    assert parse_datetime_fallback(value) == ciso8601.parse_datetime(value)
 
 
 def test_timezone(tz_env):
