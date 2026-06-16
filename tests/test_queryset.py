677962d42395a88dbe1eea063a1709433eec7c37
NoySolvin <noy@solvin.ai>
2026-06-16 13:43:11 +0300

    test(queryset): add negative controls in foreign key delete and update filters

--- tests/test_queryset.py
+++ tests/test_queryset.py
@@ -485,10 +485,13 @@ async def test_delete_filter_with_foreign_key(db):
     await Book.create(name="book1", author=author, rating=5.0)
     await Book.create(name="book2", author=author, rating=4.0)
 
+    author2 = await Author.create(name="test2")
+    await Book.create(name="book3", author=author2, rating=5.0)
+
     # This is the failing query
     await Book.filter(author__name="test").delete()
 
-    assert await Book.all().count() == 0
+    assert await Book.all().count() == 1
 
 
 @pytest.mark.asyncio
@@ -496,10 +499,16 @@ async def test_update_filter_with_foreign_key(db):
     author = await Author.create(name="test")
     await Book.create(name="book1", author=author, rating=5.0)
 
+    author2 = await Author.create(name="test2")
+    await Book.create(name="book2", author=author2, rating=5.0)
+
     await Book.filter(author__name="test").update(rating=1.0)
 
-    book = await Book.first()
+    book = await Book.get(name="book1")
     assert book.rating == 1.0
+
+    book2 = await Book.get(name="book2")
+    assert book2.rating == 5.0
 
 
 @pytest.mark.asyncio
