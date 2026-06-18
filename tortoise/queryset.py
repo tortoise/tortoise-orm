c052373104ef7682f04c94c1b3a146c3de86940f
sumit-raghav-06 <sumitraghavxc@gmail.com>
2026-06-18 19:23:29 +0530

    Apply ruff formatting

--- tortoise/queryset.py
+++ tortoise/queryset.py
@@ -1429,27 +1429,27 @@ class DeleteQuery(AwaitableQuery):
     def _make_query(self) -> None:
         self.query = copy(self.model._meta.basequery)
         self.resolve_filters()
-        
+
         # If joins are detected, rewrite into an IN subquery to avoid JOINs in DELETE
         if self._joined_tables:
             pk_column = self.model._meta.db_pk_column
-            
+
             # Create a pristine, completely separate SELECT query builder
             subquery = self._db.query_class.from_(self.model._meta.basetable).select(
                 self.model._meta.basetable[pk_column]
             )
-            
+
             # Transfer the resolved wheres and havings criteria from our filter resolution
             subquery._wheres = self.query._wheres
             subquery._havings = self.query._havings
-            
+
             # Apply joins directly to the subquery builder
             subquery._joins = self.query._joins
-            
+
             # Re-apply limits and sorting to the subquery if needed
             if self._limit:
                 subquery._limit = subquery._wrapper_cls(self._limit)
-                
+
             self.resolve_ordering(
                 model=self.model,
                 table=self.model._meta.basetable,
@@ -1457,12 +1457,10 @@ class DeleteQuery(AwaitableQuery):
                 annotations=self._annotations,
             )
             subquery._orderbys = self.query._orderbys
-            
+
             # Reconstruct clean pristine delete statement pointing to our subquery
             self.query = copy(self.model._meta.basequery)
-            self.query = self.query.where(
-                self.model._meta.basetable[pk_column].isin(subquery)
-            )
+            self.query = self.query.where(self.model._meta.basetable[pk_column].isin(subquery))
         else:
             # Traditional optimization path if no backward relations/joins exist
             if self.capabilities.support_update_limit_order_by and self._limit:
@@ -1476,7 +1474,7 @@ class DeleteQuery(AwaitableQuery):
 
         self.query._delete_from = True
         return
-    
+
     def __await__(self) -> Generator[Any, None, int]:
         self._choose_db_if_not_chosen(True)
         self._make_query()
